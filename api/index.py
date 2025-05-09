5bbf0fa698087dfb8defd5c46c49e4aef779d499
tanzheng <tanzheng@tsinghua.edu.cn>
2025-05-09 14:39:00 +0800

    开发实际的edukg.org网站

--- api/index.py
+++ api/index.py
@@ -1,52 +1,83 @@
-from flask import Flask , request
+from flask import Flask , request, render_template, send_file
 import requests , json , random
+import io
 
 
 app = Flask(__name__)
-def select(a):
-    dict ={'Anime':'a','Comic':'b','Game':'c','Literature':'d','Original':'e','Internet':'f','Other':'g','Video':'h','Poem':'i','NCM':'j','Philosophy':'k','Funny':'l'}
-    if str(a) in dict.keys() :
-        return dict[str(a)]
-    else :
-        return dict[str(random.choice(list(dict.keys())))]
 
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+@app.route('/download-presonal-graph')
+def download_presonal_graph():
+    # 你可以从磁盘读取，也可以是内存生成的内容
+    content = "id,name\n1,Alice\n2,Bob"
+    buffer = io.BytesIO()
+    buffer.write(content.encode('utf-8'))
+    buffer.seek(0)
 
+    # 返回文件
+    return send_file(
+        buffer,
+        as_attachment=True,
+        download_name='data.ttl',  # 文件名
+        mimetype='text/ttl'
+    )
 
-@app.route('/search/',methods=["GET"])
-def search():
-    query = request.args.get("s")
-    key = 'hitokoto'
-    url = requests.get("https://api.pwxiao.top/sentences/i.json")
-    text = url.text
-    data = json.loads(text)
-        
-    matches = [item for item in data if key in item and query in item[key]]
-    matches = json.dumps(matches,ensure_ascii=False)
-    return matches
-    
+@app.route('/presonal-graph',methods=["GET"])
+def presonal_graph():
+    graphs = {
+    "nodes":[
+      {
+        "id": "Basic programming concepts",
+        "name": "Basic programming concepts"
+      },
+      {
+        "id": "Understand basic Python syntax",
+        "name": "Understand basic Python syntax"
+      },
+      {
+        "id": "Introduction to Python",
+        "name": "Introduction to Python"
+      },
+      {
+        "id": "List Topics",
+        "name": "List Topics"
+      }
+    ],
+    "links":[
+      {
+        "source": "Basic programming concepts",
+        "target": "Understand basic Python syntax",
+        "relation": "hasChild"
+      },
+      {
+        "source": "Introduction to Python",
+        "target": "Understand basic Python syntax",
+        "relation": "related leaning point"
+      },
+      {
+        "source": "List Topics",
+        "target": "Introduction to Python",
+        "relation": "related resource"
+      }
+      ]
+    }
+    return render_template('personal-graph.html', json_data=graphs) 
+
+@app.route('/build-upload',methods=["GET"])
+def build_upload():
+    return render_template('builder-upload.html') 
+
+@app.route('/build-guide',methods=["GET"])
+def build_guide():
+    return render_template('builder-guide.html')    
+
+@app.route('/example',methods=["GET"])
+def example():
+    return render_template('example.html')    
 
 
 @app.route('/',methods=["GET"])
-def return_OneText():
+def home():
+    return render_template('index.html')
 
-    category = request.args.get("category")
-
-    category =  select(category)
-    url = requests.get("https://api.pwxiao.top/sentences/" + category + ".json")
-    text = url.text
-    # with open("../sentences/"+category+".json",'r',encoding='utf-8') as f:
-    #      text = f.read()       
-
-    res = json.loads(text)
-
-    try:
-        number = random.randint(0,(len(res) - 1))
-    except:
-        number = 6
-
-    result = res[number]
-    fina_res = json.dumps(result,ensure_ascii=False)
- 
-    return fina_res
+if __name__ == '__main__':
+    app.run(port=8081, host="0.0.0.0", debug=True)  
